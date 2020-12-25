7a356f516395888eabf84c109ad0745001ce3b13
IamSanketMore <sanketmore.999.m@gmail.com>
2020-12-25 23:41:10 +0530

    Add: Sample Class Program In master branch

---
+++ SampleClassProgram.java
@@ -0,0 +1,10 @@
+public class SampleClassProgram
+{
+	public static void main(String args[])
+	{
+               	System.out.println("--------------------------------------------------------------------");
+                System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+                System.out.println("--------------------------------------------------------------------");
+
+	}
+}
